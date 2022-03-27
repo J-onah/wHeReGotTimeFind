173578787c8e94ba0a35a93caf95e9ba0f6110b1
Ian Goh <ian@iangohy.com>
2022-03-28 00:13:28 +0800

    Added doc comments

--- app/src/main/java/com/example/wheregottimefind/backendAPI/BackendApi.java
+++ app/src/main/java/com/example/wheregottimefind/backendAPI/BackendApi.java
@@ -8,15 +8,31 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+/**
+ * Class to aid in obtaining data from the backend/database
+ */
+
 public class BackendApi {
     private static final String API_ENDPOINT = "";
     private static final String TAG = "backend_api";
     private static Call<FullReviewResponse> call;
 
+    /**
+     * Asynchronously obtains reviews from database, searched by name
+     *
+     * @param updater an object implementing the AsyncUpdate updater, called on data received
+     */
+
     public static void getReviewsByName(AsyncUpdate updater) {
         getData("getReviewsByName", updater);
     }
 
+    /**
+     * Asynchronously obtains reviews from database
+     *
+     * @param queryType string representing the type of query to obtain
+     * @param updater   an object implementing the AsyncUpdate updater, called on data received
+     */
     
     private static void getData(String queryType, AsyncUpdate updater) {
         Retrofit retrofit = new Retrofit.Builder()
