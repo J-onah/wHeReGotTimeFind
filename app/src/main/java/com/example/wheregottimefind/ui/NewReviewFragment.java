2ac24136a869956797078673fbda0a9eea656b2d
Ian Goh <ian@iangohy.com>
2022-04-01 17:03:13 +0800

    Changed product name to auto complete

--- app/src/main/java/com/example/wheregottimefind/ui/NewReviewFragment.java
+++ app/src/main/java/com/example/wheregottimefind/ui/NewReviewFragment.java
@@ -1,4 +1,4 @@
-package com.example.wheregottimefind;
+package com.example.wheregottimefind.ui;
 
 import android.os.Bundle;
 
@@ -6,7 +6,6 @@ import androidx.fragment.app.Fragment;
 
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,13 +14,12 @@ import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 
+import com.example.wheregottimefind.R;
 import com.example.wheregottimefind.backendAPI.AsyncUpdate;
 import com.example.wheregottimefind.backendAPI.BackendApi;
-import com.example.wheregottimefind.data.pojo.FullReview;
+import com.example.wheregottimefind.data.pojo.Product;
 import com.example.wheregottimefind.data.pojo.Vendor;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
-import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -31,21 +29,15 @@ import java.util.ArrayList;
 public class NewReviewFragment extends Fragment {
     final static String TAG = "new_review_fragment";
     FloatingActionButton fab;
-    EditText vendor_location_edittext;
-    EditText vendor_phone_no_edittext;
-
-    private static final ArrayList<Vendor> vendors = new ArrayList<>();
-
+    EditText vendorLocationEdittext;
+    EditText vendorPhoneEditText;
 
     public NewReviewFragment() {
         // Required empty public constructor
-        // For testing only
-//        this.updateVendors();
     }
 
     private static void updateVendors(String vendorName, ArrayAdapter<Vendor> adapter,
                                       AutoCompleteTextView vendorNameTextView) {
-        // TODO: Update to call API with query
         BackendApi.getVendorsByVendorName(vendorName, new AsyncUpdate<Vendor>() {
             @Override
             public void updateOnDataReceived(Vendor[] vendorsData) {
@@ -58,13 +50,12 @@ public class NewReviewFragment extends Fragment {
                 }
             }
         });
-
-        // THIS IS MOCK DATA
-//        vendors.clear();
-//        vendors.add(new Vendor("vendor1", "90 somapah road", 91234567, 2));
-//        vendors.add(new Vendor("goodvendor", "1 jurong road", 90909090, 3));
-//        vendors.add(new Vendor("badvendor", "30 Kallang Road", 65656565, 4));
     }
+
+    private static void updateProducts() {
+
+    }
+
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
@@ -95,8 +86,8 @@ public class NewReviewFragment extends Fragment {
         rootView = inflater.inflate(R.layout.fragment_newreview, container, false);
 
         // Get reference to edittext
-        vendor_location_edittext = rootView.findViewById(R.id.vendor_location);
-        vendor_phone_no_edittext = rootView.findViewById(R.id.vendor_phone_no);
+        vendorLocationEdittext = rootView.findViewById(R.id.vendor_location);
+        vendorPhoneEditText = rootView.findViewById(R.id.vendor_phone_no);
 
 
         // Hide FAB when leaving New Review page
@@ -105,9 +96,13 @@ public class NewReviewFragment extends Fragment {
 
         // Autocomplete field
         AutoCompleteTextView vendorNameTextView = rootView.findViewById(R.id.vendor_name);
-        ArrayAdapter<Vendor> adapter = new ArrayAdapter<Vendor>(container.getContext(),
-                android.R.layout.simple_list_item_1, vendors);
-        vendorNameTextView.setAdapter(adapter);
+        AutoCompleteTextView productNameTextView = rootView.findViewById(R.id.review_product_name);
+        ArrayAdapter<Vendor> vendorAdapter = new ArrayAdapter<Vendor>(container.getContext(),
+                android.R.layout.simple_list_item_1);
+        ArrayAdapter<Product> productAdapter = new ArrayAdapter<>(container.getContext(),
+                android.R.layout.simple_list_item_1);
+        vendorNameTextView.setAdapter(vendorAdapter);
+        productNameTextView.setAdapter(productAdapter);
 
         vendorNameTextView.addTextChangedListener(new TextWatcher() {
             @Override
@@ -116,7 +111,7 @@ public class NewReviewFragment extends Fragment {
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                updateVendors(charSequence.toString(), adapter, vendorNameTextView);
+                updateVendors(charSequence.toString(), vendorAdapter, vendorNameTextView);
             }
 
             @Override
@@ -130,13 +125,16 @@ public class NewReviewFragment extends Fragment {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Vendor clicked_vendor = ((Vendor) adapterView.getItemAtPosition(i));
-                Log.i(TAG, "Autofill item clicked: " + clicked_vendor);
 
                 // Fill in other fields
-                vendor_location_edittext.setText(clicked_vendor.getLocation());
-                vendor_phone_no_edittext.setText(String.valueOf(clicked_vendor.getPhone_no()));
-                vendor_location_edittext.setEnabled(false);
-                vendor_phone_no_edittext.setEnabled(false);
+                vendorLocationEdittext.setText(clicked_vendor.getLocation());
+                vendorPhoneEditText.setText(String.valueOf(clicked_vendor.getPhone_no()));
+                vendorLocationEdittext.setEnabled(false);
+                vendorPhoneEditText.setEnabled(false);
+
+                // Set vendor id as tag to name field
+                vendorNameTextView.setTag(Integer.valueOf(clicked_vendor.getId()));
+                System.out.println("TAG null?: " + vendorNameTextView.getTag());
 
                 // TODO: Set details for form submission
             }
@@ -151,8 +149,10 @@ public class NewReviewFragment extends Fragment {
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                vendor_location_edittext.setEnabled(true);
-                vendor_phone_no_edittext.setEnabled(true);
+                vendorLocationEdittext.setEnabled(true);
+                vendorPhoneEditText.setEnabled(true);
+                vendorNameTextView.setTag(null);
+                System.out.println("TAG null?: " + vendorNameTextView.getTag());
             }
 
             @Override
